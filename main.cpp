627f9e5f454fd4145f776102d7a9ea35be7b2943
Roman Ravas <romorko@gmail.com>
2020-01-11 20:50:27 +0100

    druhy pokus

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <list>
+#include <algorithm>
+#include <iterator>
+
+using std::cout;
+using std::cin;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout<<"Zadaj pocet cisel:";
+    int pocet=50;
+    int i=1;
+    //cin>>pocet;
+    std::list<int> cisla(pocet);
+    std::generate(cisla.begin(),cisla.end(),[&i](){return ++i;});
+    std::copy(cisla.begin(),cisla.end(),std::ostream_iterator<int>(cout," "));
+    cout<<std::endl;
+    auto prvy =cisla.begin();
+    while(prvy!=cisla.end())
+    {
+        cout<<*prvy<<" ";
+        cisla.remove_if([prvy](auto prvok){return prvok%(*prvy)==0;});
+        //std::copy(cisla.begin(),cisla.end(),std::ostream_iterator<int>(cout," "));
+        //cout<<std::endl;
+        prvy=cisla.begin();
+    }
+    std::copy(cisla.begin(),cisla.end(),std::ostream_iterator<int>(cout," "));
+return 0;
 }
