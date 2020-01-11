b8ac1300d4dc3f4228ea4ff01ee2777dc2905587
Roman Ravas <romorko@gmail.com>
2020-01-11 21:17:22 +0100

    treti pokus

--- main.cpp
+++ main.cpp
@@ -8,23 +8,20 @@ using std::cin;
 
 int main()
 {
-    cout<<"Zadaj pocet cisel:";
-    int pocet=50;
-    int i=1;
+    cout << "Zadaj pocet cisel:";
+    int pocet = 100;
+    int i = 1;
     //cin>>pocet;
     std::list<int> cisla(pocet);
-    std::generate(cisla.begin(),cisla.end(),[&i](){return ++i;});
-    std::copy(cisla.begin(),cisla.end(),std::ostream_iterator<int>(cout," "));
-    cout<<std::endl;
-    auto prvy =cisla.begin();
-    while(prvy!=cisla.end())
+    std::generate(cisla.begin(), cisla.end(), [&i]() { return ++i; });
+    std::copy(cisla.begin(), cisla.end(), std::ostream_iterator<int>(cout, " "));
+    cout << std::endl;
+    auto prvy = cisla.begin();
+    while (prvy != cisla.end())
     {
-        cout<<*prvy<<" ";
-        cisla.remove_if([prvy](auto prvok){return prvok%(*prvy)==0;});
-        //std::copy(cisla.begin(),cisla.end(),std::ostream_iterator<int>(cout," "));
-        //cout<<std::endl;
-        prvy=cisla.begin();
+        cout << *prvy << " ";
+        cisla.remove_if([prvy](auto prvok) { return !(prvok % (*prvy)); });
+        prvy = cisla.begin();
     }
-    std::copy(cisla.begin(),cisla.end(),std::ostream_iterator<int>(cout," "));
-return 0;
+    return 0;
 }
